a1e0ee52682c6a5c04d3a7416f2955b25453c7f4
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2023-06-26 07:34:18 -0500

    initial commit

---
+++ interfaces/site-config.interface.ts
@@ -0,0 +1,35 @@
+import * as types from '../lib/types'
+
+export interface SiteConfig {
+  rootNotionPageId: string
+  rootNotionSpaceId?: string
+
+  name: string
+  domain: string
+  author: string
+  description?: string
+  language?: string
+
+  defaultPageIcon?: string | null
+  defaultPageCover?: string | null
+  defaultPageCoverPosition?: number | null
+
+  isRedisEnabled?: boolean
+
+  isPreviewImageSupportEnabled?: boolean
+  isTweetEmbedSupportEnabled?: boolean
+  isSearchEnabled?: boolean
+
+  includeNotionIdInUrls?: boolean
+  pageUrlOverrides?: types.PageUrlOverridesMap
+  pageUrlAdditions?: types.PageUrlOverridesMap
+
+  navigationStyle?: types.NavigationStyle
+  navigationLinks?: Array<NavigationLink>
+}
+
+export interface NavigationLink {
+  title: string
+  pageId?: string
+  url?: string
+}
---
+++ lib/site-config.ts
@@ -0,0 +1,5 @@
+import type { SiteConfig } from "interfaces/site-config.interface"
+
+export const siteConfig = (config: SiteConfig): SiteConfig => {
+  return config
+}
